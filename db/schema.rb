c718675e0a4073a22890d85e673099ccff24da9f
crystal <noelani.villanueva@gmail.com>
2021-06-27 15:16:06 -0700

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_06_27_211839) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "notes", force: :cascade do |t|
     t.integer "user_id"
     t.string "title"
